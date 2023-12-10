07c5f05a5a5c54cb75eb554d035c6cf62cbe8179
Maria Pandele <maria.pandele33@gmail.com>
2023-12-10 20:12:20 +0100

    Finish aoc2023-day9

--- AdventOfCode2023/day9/src/main.rs
+++ AdventOfCode2023/day9/src/main.rs
@@ -15,11 +15,54 @@ fn read_input() -> Vec<Vec<i64>> {
         .collect()
 }
 
+fn compute_pascal_triangle(n: i64) -> Vec<Vec<i64>> {
+    let mut pascal_triangle: Vec<Vec<i64>> = Vec::new();
+    for i in 0..n {
+        let mut row: Vec<i64> = Vec::new();
+        for j in 0..(i + 1) {
+            if j == 0 || j == i {
+                row.push(1);
+            } else {
+                row.push(pascal_triangle[(i - 1) as usize][(j - 1) as usize] + pascal_triangle[(i - 1) as usize][j as usize]);
+            }
+        }
+        pascal_triangle.push(row);
+    }
+    pascal_triangle
+}
+
+fn predict_next(history: &Vec<i64>, pascal_triangle: &Vec<Vec<i64>>) -> i64{
+    let mut ans: i64 = 0;
+    let n = history.len();
+    for i in 0..history.len() {
+        for j in 0..(i + 1) {
+            let number = (if j % 2 == 0 {1} else {-1}) * history[n - j - 1] * pascal_triangle[i][j];
+            ans += number;
+        }
+    }
+    println!("{} {}", history.len(), ans);
+    ans
+}
+
+fn predict_prev(history: &Vec<i64>, pascal_triangle: &Vec<Vec<i64>>) -> i64{
+    let mut ans: i64 = 0;
+    let n = history.len();
+    for i in 0..history.len() {
+        for j in 0..(i + 1) {
+            let number = (if j % 2 == 0 {1} else {-1}) * history[j] * pascal_triangle[i][j];
+            ans += number;
+        }
+    }
+    println!("{} {}", history.len(), ans);
+    ans
+}
+
 fn main() {
     let histories = read_input();
-    let ans: i64 = 0;
+    let mut ans: i64 = 0;
+    let pascal_triangle = compute_pascal_triangle(30);
     for history in histories {
-        ans += predict_next(history);
+        ans += predict_prev(&history, &pascal_triangle);
     }
     println!("Answer: {}", ans);
 }
